6681b4a1c498a66262be352112ae037d4b877a82
Shreyas Taware <shreyastaware13@gmail.com>
2020-10-18 12:12:00 +0530

    commit

--- server/util.py
+++ server/util.py
@@ -34,12 +34,12 @@ def load_saved_artifacts():
     global __data_columns
     global __locations
 
-    with open("E:/1 MOOCs/2 Skills/Python/python_practice/BHP/server/artifacts/columns.json", "r") as f:
+    with open("./artifacts/columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
 
     global __model
-    with open("E:/1 MOOCs/2 Skills/Python/python_practice/BHP/server/artifacts/banglore_home_prices_model.pickle", "rb") as f:
+    with open("./artifacts/banglore_home_prices_model.pickle", "rb") as f:
         __model = pickle.load(f)
     print("loading saved artifacts...done")
 
